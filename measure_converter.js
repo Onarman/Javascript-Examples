b00532cd452902737a4f2f1ebf25733864827cfa
onarman <onarmannn@gmail.com>
2022-06-03 20:25:11 +0300

    step-11

---
+++ measure_converter.js
@@ -0,0 +1,11 @@
+//MEASURE CONVERTER
+/*
+ program that asks the user to enter Celsius temperature (it can be a decimal number),\
+ converts the entered temperature into Fahrenheit degree and console the result.
+*/
+
+
+let celsisus=prompt("Please enter a temperature ('C') : ");
+
+fahrenheit=(1.8*Number(celsisus))+32;
+console.log(`Oda sıcaklığı ${fahrenheit}.F seviyesindedir.`)
