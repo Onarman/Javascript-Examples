20c43b30ed0b85e54058b2b228ad6b82812221da
onarman <onarmannn@gmail.com>
2022-06-03 20:24:21 +0300

    step-9

---
+++ last_man_standing.js
@@ -0,0 +1,12 @@
+// function lastManStanding(last_number){
+//     var arr = [];
+//     for (let i = 0; i < last_number; i++) arr.push(i+1);
+// }
+// console.log(arr[1,2,3,4,5,6,7,8,9]);
+
+
+function sum(a,b){
+    return a + b;
+
+}
+console.log(sum(2,3));
